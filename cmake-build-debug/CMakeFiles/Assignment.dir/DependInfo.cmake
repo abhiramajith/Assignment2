e9fd217200a235fecc29091a62b873f3039cda76
abhiramajith <abhiram.ajith@ucdconnect.ie>
2021-05-18 11:46:26 +0100

    Final_Version

---
+++ cmake-build-debug/CMakeFiles/Assignment.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/Abhiram/C_programs/Assignment/anyMove.c" "/cygdrive/c/Users/Abhiram/C_programs/Assignment/cmake-build-debug/CMakeFiles/Assignment.dir/anyMove.c.o"
+  "/cygdrive/c/Users/Abhiram/C_programs/Assignment/endGame.c" "/cygdrive/c/Users/Abhiram/C_programs/Assignment/cmake-build-debug/CMakeFiles/Assignment.dir/endGame.c.o"
+  "/cygdrive/c/Users/Abhiram/C_programs/Assignment/gameInfo.c" "/cygdrive/c/Users/Abhiram/C_programs/Assignment/cmake-build-debug/CMakeFiles/Assignment.dir/gameInfo.c.o"
+  "/cygdrive/c/Users/Abhiram/C_programs/Assignment/initialiseGame.c" "/cygdrive/c/Users/Abhiram/C_programs/Assignment/cmake-build-debug/CMakeFiles/Assignment.dir/initialiseGame.c.o"
+  "/cygdrive/c/Users/Abhiram/C_programs/Assignment/isSandwich.c" "/cygdrive/c/Users/Abhiram/C_programs/Assignment/cmake-build-debug/CMakeFiles/Assignment.dir/isSandwich.c.o"
+  "/cygdrive/c/Users/Abhiram/C_programs/Assignment/main.c" "/cygdrive/c/Users/Abhiram/C_programs/Assignment/cmake-build-debug/CMakeFiles/Assignment.dir/main.c.o"
+  "/cygdrive/c/Users/Abhiram/C_programs/Assignment/playerMove.c" "/cygdrive/c/Users/Abhiram/C_programs/Assignment/cmake-build-debug/CMakeFiles/Assignment.dir/playerMove.c.o"
+  "/cygdrive/c/Users/Abhiram/C_programs/Assignment/validMoveChecker.c" "/cygdrive/c/Users/Abhiram/C_programs/Assignment/cmake-build-debug/CMakeFiles/Assignment.dir/validMoveChecker.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Assignment.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "Assignment.exe"
+  "Assignment.pdb"
+  "CMakeFiles/Assignment.dir/anyMove.c.o"
+  "CMakeFiles/Assignment.dir/endGame.c.o"
+  "CMakeFiles/Assignment.dir/gameInfo.c.o"
+  "CMakeFiles/Assignment.dir/initialiseGame.c.o"
+  "CMakeFiles/Assignment.dir/isSandwich.c.o"
+  "CMakeFiles/Assignment.dir/main.c.o"
+  "CMakeFiles/Assignment.dir/playerMove.c.o"
+  "CMakeFiles/Assignment.dir/validMoveChecker.c.o"
+  "libAssignment.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
